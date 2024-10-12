f727fef28b11096c893e7c9a04c0ded84261d52c
Nazashei <victorfabianmancini@gmail.com>
2024-10-12 15:43:34 -0300

    Sube tutorial Diez Dias Despues Intro

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const videoLinks = [
     { url: "https://www.youtube.com/watch?v=H-O_0ssVs90", title: "Solo Aquí", thumbnail: "./Images/soloAqui.jpg" },
     { url: "https://www.youtube.com/watch?v=Xz5UqJOpSLw", title: "Intoxicarme", thumbnail: "./Images/intoxicarme.webp" },
     { url: "https://www.youtube.com/watch?v=_hCUGKId9Sk", title: "Cadenas", thumbnail: "./Images/voragine.webp" },
+    { url: "https://www.youtube.com/watch?v=kll5b0bYi3Q", title: "Apocalipsis Confort", thumbnail: "./Images/huracan.webp" },
     { url: "https://www.youtube.com/watch?v=mg4U3QjYqww", title: "Huracan", thumbnail: "./Images/huracan.webp" },
     { url: "https://www.youtube.com/watch?v=XDsYh91e35s&pp=ygUVbGliZXJ0YWQgY292ZXIgYWlyYmFn", title: "Libertad", thumbnail: "./Images/libertad.jpg" },
     { url: "https://www.youtube.com/watch?v=M63VNT56CWE", title: "Kamikaze", thumbnail: "./Images/alpa.jpg" },
@@ -11,6 +12,7 @@ const videoLinks = [
     { url: "https://www.youtube.com/watch?v=kF4hPxj6MPo", title: "Mecanismos de control", thumbnail: "./Images/voragine.webp" },
     { url: "https://www.youtube.com/watch?v=o4b3xCPgfHc", title: "Relampagos", thumbnail: "./Images/huracan.webp" },
     { url: "https://www.youtube.com/watch?v=BAHJiB-BXKk", title: "Cuchillos Guantanamera", thumbnail: "./Images/alpa.jpg" },
+    { url: "https://youtu.be/kF4hPxj6MPo", title: "Estado Salvaje", thumbnail: "./Images/voragine.webp" },
     { url: "https://www.youtube.com/watch?v=AUjEHYpW2A0", title: "Motor Enfermo", thumbnail: "./Images/alpa.jpg" },
     { url: "https://www.youtube.com/watch?v=EXQVC1viLW0", title: "Diez Dias Despues", thumbnail: "./Images/voragine.webp" },
     { url: "https://www.youtube.com/watch?v=kj_x7q6Vvic", title: "Perdido", thumbnail: "./Images/alpa.jpg" },
@@ -28,13 +30,17 @@ const videoLinks = [
     { url: "https://www.youtube.com/watch?v=IFagRLy-G4Q", title: "Bajos Instintos", thumbnail: "./Images/voragine.webp" },
     { url: "https://www.youtube.com/watch?v=j43ROkzAmTQ&t=4s", title: "Mujer Amante", thumbnail: "./Images/mujer.jpg" },
     { url: "https://www.youtube.com/watch?v=Qh47eaKWXvA", title: "El Ultimo Ataque", thumbnail: "./Images/rata.jpg" },
-    { url: "https://www.youtube.com/watch?v=JDhixIol9-4", title: "Preludio Obsesivo", thumbnail: "./Images/rata.jpg" },
-    { url: "https://www.youtube.com/watch?v=kll5b0bYi3Q", title: "Apocalipsis Confort", thumbnail: "./Images/huracan.webp" }
+    { url: "https://www.youtube.com/watch?v=JDhixIol9-4", title: "Preludio Obsesivo", thumbnail: "./Images/rata.jpg" }
 ];
+const lessonLinks = [
+    { url: "https://youtu.be/cWuVUw0lTY0", title: "Intro Diez Dias Despues", thumbnail: "./Images/IntroVelezDiez.webp" },
+]
 
 let currentVideoIndex = 0;
 let videosPerPage = 1; 
 
+let currentLessonIndex = 0;
+let lessonPerPage = 1;
 
 function adjustVideosPerPage() {
     const screenWidth = window.innerWidth;
@@ -45,6 +51,18 @@ function adjustVideosPerPage() {
         videosPerPage = 2; 
     } else {
         videosPerPage = 4; 
+    }
+}
+
+function adjustLessonPerPage() {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 600) {
+        lessonPerPage = 1; 
+    } else if (screenWidth >= 600 && screenWidth < 1024) {
+        lessonPerPage = 2; 
+    } else {
+        lessonPerPage = 4; 
     }
 }
 
@@ -78,10 +96,48 @@ function updateVideoDisplay(filteredVideos = videoLinks) {
     }
 }
 
+
+function updateLessonDisplay(filteredLesson = lessonLinks) {
+    const lesson = document.querySelector('.lesson-container');
+    lesson.innerHTML = '';
+
+    const start = currentLessonIndex;
+    const end = Math.min(currentLessonIndex + lessonPerPage, filteredLesson.length);
+
+    for (let i = start; i < end; i++) {
+        const lessonItem = document.createElement('div');
+        lessonItem.classList.add('lesson-item');
+
+        const lessonLink = document.createElement('a');
+        lessonLink.href = filteredLesson[i].url;
+        lessonLink.target = "_blank";
+
+        const thumbnail = document.createElement('img');
+        thumbnail.src = filteredLesson[i].thumbnail;
+        thumbnail.alt = filteredLesson[i].title;
+        thumbnail.classList.add('lesson-thumbnail');
+
+        const title = document.createElement('h3');
+        title.textContent = filteredLesson[i].title;
+
+        lessonLink.appendChild(thumbnail);
+        lessonLink.appendChild(title);
+        lessonItem.appendChild(lessonLink);
+        lesson.appendChild(lessonItem);
+    }
+}
+
 document.getElementById('prevButton').addEventListener('click', () => {
     if (currentVideoIndex > 0) {
         currentVideoIndex -= videosPerPage;
         updateVideoDisplay();
+    }
+});
+
+document.getElementById('prevButtonNavigation').addEventListener('click', () => {
+    if (currentLessonIndex > 0) {
+        currentLessonIndex -= lessonPerPage;
+        updateLessonDisplay(); 
     }
 });
 
@@ -91,6 +147,14 @@ document.getElementById('nextButton').addEventListener('click', () => {
         updateVideoDisplay();
     }
 });
+
+document.getElementById('nextButtonNavigation').addEventListener('click', () => {
+    if (currentLessonIndex + lessonPerPage < lessonLinks.length) {
+        currentLessonIndex += lessonPerPage;
+        updateLessonDisplay(); 
+    }
+});
+
 
 
 document.getElementById('search').addEventListener('input', () => {
@@ -104,7 +168,12 @@ document.getElementById('search').addEventListener('input', () => {
 window.addEventListener('resize', () => {
     adjustVideosPerPage();
     updateVideoDisplay();
+    adjustLessonPerPage();
+    updateLessonDisplay();
 });
+
 
 adjustVideosPerPage();
 updateVideoDisplay();
+adjustLessonPerPage();
+updateLessonDisplay();
